53ec116d0c7b81b8314aa520a86388569d530e87
AHSimoC1 <ahsimoc1@srv020.img.med.uni-tuebingen.de>
2025-07-07 13:48:32 +0200

    test

--- scripts/prepare_inputs.py
+++ scripts/prepare_inputs.py
@@ -28,3 +28,42 @@ subset_df.index.name = "protein_ID"
 
 #save results
 subset_df.to_csv("../output/quant_input.tsv", sep="\t")
+
+
+
+####################### to delete
+
+protein_file = "test_1-6/proteinGroups.txt"
+mapping_file = "test_1-6/mapping_table_1-6.tsv"
+
+#read the MaxQuant file with protein intensities 
+protein_df = pd.read_csv(protein_file, sep="\t", low_memory=False)
+
+mapping_df = pd.read_csv(mapping_file, sep="\t")
+
+
+subset_df.to_csv("../output/input_1-6.tsv", sep="\t")
+
+# all
+protein_file = "data/test_all/proteinGroups.txt"
+mapping_file = "data/test_all/mapping_table_all.tsv"
+
+#read the MaxQuant file with protein intensities 
+protein_df = pd.read_csv(protein_file, sep="\t", low_memory=False)
+
+mapping_df = pd.read_csv(mapping_file, sep="\t")
+
+
+subset_df.to_csv("output/input_all.tsv", sep="\t")
+
+# w HEK
+protein_file = "data/test_w_hek/proteinGroups.txt"
+mapping_file = "data/test_w_hek/mapping_table_w_hek.tsv"
+
+#read the MaxQuant file with protein intensities 
+protein_df = pd.read_csv(protein_file, sep="\t", low_memory=False)
+
+mapping_df = pd.read_csv(mapping_file, sep="\t")
+
+
+subset_df.to_csv("output/input_w_kek.tsv", sep="\t")
